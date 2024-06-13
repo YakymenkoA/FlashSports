eecc5ad0e5f7abfadda6b39d6aba9512939ce110
Jabrail <Jabrail.falcon@gmail.com>
2024-06-13 17:24:14 +0400

    Interface, Repositories, Criptography

---
+++ Client/Repositories/ClientRepository.cs
@@ -0,0 +1,45 @@
+﻿using Client.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.Repositories
+{
+    internal class ClientRepository : IClient
+    {
+        private int _port;
+        private IPAddress _addr;
+        private IPEndPoint _ep;
+        
+        public void Authorization(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(string ip, string port)
+        {
+            try
+            {
+                _addr = IPAddress.Parse(ip);
+                _port = int.Parse(port);
+                _ep = new IPEndPoint(_addr, _port);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"\n> Connection Error:\n  {ex.Message}", "Error",
+                  MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        public void Registration(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
